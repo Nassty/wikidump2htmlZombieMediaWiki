644f6a35b74d63e2915dfa4feea9a8eb6f59ff43
Mariano Guerra <luismarianoguerra@gmail.com>
2010-11-07 22:53:08 -0300

    fix bugs in sender.py

--- sender.py
+++ sender.py
@@ -26,19 +26,19 @@ class HTTPSender(object):
         My *AWESOME* constructor
         """
 
-        self.url = host 
+        self.host = host
         self.path = path
         self.uri = uri
-        self.port = port
+        self.port = int(port)
 
-    def get_xml_files(path):
+    def get_xml_files(self, path):
         """
         returns all the xml files in the given path
         """
 
         return glob.glob("%s/*.xml" % path)
 
-    def send_file(filename):
+    def send_file(self, filename):
         """
         Receives a filename and performs an HTTP Query against the server
         Creates an html file with the same name as the received and
@@ -58,30 +58,29 @@ class HTTPSender(object):
         origin_data = origin_file.read()
         origin_file.close()
 
-        params = urllib.urlencode(dict(text=origin_data))
-        headers = {"Content-type": "application/x-www-form-urlencoded", 
+        headers = {"Content-type": "text/plain",
                    "Accept": "text/html"}
 
-        conn = httplib.HTTPConnection("%s:%s" %(self.host, self.port))
-        conn.request("POST", self.uri, params, headers)
+        conn = httplib.HTTPConnection(self.host, self.port)
+        conn.request("POST", self.uri, origin_data, headers)
 
         response = conn.getresponse()
-        
+
         if response.status == 200:
             result_file = open(result_name, "w")
             result_file.write(response.read())
             result_file.close()
 
             return True
-
-        raise SenderException(response.reason)
+        else:
+            raise SenderException(response.reason)
 
     def run(self):
         """
-        Runs sequentially a file list against send_file 
+        Runs sequentially a file list against send_file
 
         may be interrupted by a SenderException :(
-        (lets try not to) 
+        (lets try not to)
         """
         map(self.send_file, self.get_xml_files(self.path))
         return True
@@ -91,14 +90,14 @@ def main():
     options, args = parse_options()
 
     HTTPSender(options.host, options.uri, options.path,
-               options.port).run() 
-    
+               options.port).run()
+
 def parse_options():
     """parse and return command line options"""
     parser = OptionParser()
-    parser.add_option("-H", "--host", dest="host", 
+    parser.add_option("-H", "--host", dest="host",
                       help="mediawiki host", metavar="HOST")
-    parser.add_option("-u", "--uri", dest="uri", default="dumper.php",
+    parser.add_option("-u", "--uri", dest="uri", default="/mediawiki/dumper.php",
                       help="URI for the parser file", metavar="URI")
     parser.add_option("-p", "--port", dest="port", default=80,
                       help="port for the mediawiki", metavar="PORT")
